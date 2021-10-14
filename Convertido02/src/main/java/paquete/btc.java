e24410a67ba30a19c540e4e6b1d9dfe40e564ae9
Eduardo Jiménez <67086521+edujim@users.noreply.github.com>
2021-10-14 14:24:32 -0600

    Add files via upload

---
+++ Convertido02/src/main/java/paquete/btc.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paquete;
+import java.awt.Panel;
+/**
+ *
+ * @author Usuario
+ */
+public class btc {
+  public static void main(String[] arg){  
+    Panel j=new Panel ();
+        j.setVisible(true);
+    }
+}
